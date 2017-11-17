7a5184c9ae82f4f0ba1ec3501b7e163746abb346
Mads Schultz <s175123@student.dtu.dk>
2017-11-17 10:29:21 +0100

    Test kommentar

--- 36_del3/src/main/Main.java
+++ 36_del3/src/main/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
 		System.out.println("test 123");
 		System.out.println("Hello World!");
+		
 		System.out.println("test jacob");
 		System.out.println("test monica 123");
+		//test 123
+		
 		
 	}
 
