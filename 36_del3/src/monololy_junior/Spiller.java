e239692080cbac34e6f4da8eaa6e3221272b69d9
Jacob <Jacob@DESKTOP-91SF1OG>
2017-11-19 15:37:35 +0100

    Tilføjet Spillerklassen, og lavet enkel get og set metoder

---
+++ 36_del3/src/monololy_junior/Spiller.java
@@ -0,0 +1,24 @@
+package monololy_junior;
+
+public class Spiller {
+	private String spillerNavn;
+	private Felt[] ejetFelter;
+	private boolean erDød;
+	
+	public Spiller() {
+		erDød = false;
+	}
+
+	public String getSpillerNavn() {
+		return spillerNavn;
+	}
+
+	public void setSpillerNavn(String spillerNavn) {
+		this.spillerNavn = spillerNavn;
+	}
+	
+	
+	
+	
+
+}
