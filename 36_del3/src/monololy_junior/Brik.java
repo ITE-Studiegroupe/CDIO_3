8234e1b6a2766394cbeb6e13562b8f19c08195f0
Jacob <Jacob@DESKTOP-91SF1OG>
2017-11-19 16:05:11 +0100

    Oprette en brik klasse med tilhørende set og get metoder

---
+++ 36_del3/src/monololy_junior/Brik.java
@@ -0,0 +1,23 @@
+package monololy_junior;
+
+import java.awt.Color;
+
+public class Brik {
+	private int placering; 
+	private Color farve;
+	public int getPlacering() {
+		return placering;
+	}
+	public void setPlacering(int placering) {
+		this.placering = placering;
+	}
+	public Color getFarve() {
+		return farve;
+	}
+	public void setFarve(Color farve) {
+		this.farve = farve;
+	}
+	
+	
+
+}
