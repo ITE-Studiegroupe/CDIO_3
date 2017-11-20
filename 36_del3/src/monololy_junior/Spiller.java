af968d3ed0750cdf40f1a68a6f39c65a0bd1b407
nicol <nicol@LAPTOP-KMKH3SE0.eduroam.wireless.dtu.dk>
2017-11-20 12:22:05 +0100

    Spiller

--- 36_del3/src/monololy_junior/Spiller.java
+++ 36_del3/src/monololy_junior/Spiller.java
@@ -1,27 +1,36 @@
 package monololy_junior;
-
+import java.awt.Color;
+import java.util.Arrays;
 
 public class Spiller {
 	private String spillerNavn;
-	private int[] ejetFelter;
+	private Felt[] ejetFelter;
 	private Konto konto;
 	private Brik brik;
-	
-	
+
 	public String getSpillerNavn() {
 		return spillerNavn;
 	}
+
 	public void setSpillerNavn(String spillerNavn) {
 		this.spillerNavn = spillerNavn;
 	}
-	public void tilføjFelt() {
-		
+
+	public void tilføjFelt(Felt spillerFelt) {
+		Felt[] nytFelt = Arrays.copyOf(ejetFelter, ejetFelter.length + 1);
+		nytFelt[nytFelt.length-1] = spillerFelt;
+		Felt[] ejetFelter = nytFelt;
 	}
 	
-
-	
-	
-	
-
-	
+	public void checkToEns(Felt købtFelt) {
+		for(int i = 1;i<ejetFelter.length;i++) {
+			Color farve1 = ejetFelter[i].getFeltFarve();
+			Color farve2 = købtFelt.getFeltFarve();
+			if(farve1.getRGB()==farve2.getRGB()) {
+				int købtFeltVærdi = købtFelt.getVærdi()*2;
+				købtFelt.setVærdi(købtFeltVærdi);
+				ejetFelter[i].setVærdi(købtFeltVærdi);
+			}
+		}
 	}
+}
