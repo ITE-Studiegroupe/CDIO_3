28d37766bb8b5932b8da91eefb88c191a51e1725
Monica Schmidt <Monica Schmidt@DESKTOP-TU5PO0V.eduroam.wireless.dtu.dk>
2017-11-20 11:26:16 +0100

    Igang med at oprette klassen Plade

--- 36_del3/src/monololy_junior/Plade.java
+++ 36_del3/src/monololy_junior/Plade.java
@@ -1,11 +1,31 @@
 package monololy_junior;
 
+import java.awt.Color;
+
 public class Plade {
 	private int antalFelter;
 	private String[] feltNavn;
+	private Chancekort[] chancekort;
+	private Felt[] felter = new Felt[24];
+
+	//Bygfelter metode
+	public void bygFelter() {
+		felter[0] = new Felt();
+		felter[0].setFeltFarve(Color.WHITE);
+		felter[0].setVærdi(0);
+		
+		
 	
-	public void bygPlade() {
+		
+		
+		
+		
+		
 		
 	}
+	
+	//Chancekort metode
+	
+	
 
 }
