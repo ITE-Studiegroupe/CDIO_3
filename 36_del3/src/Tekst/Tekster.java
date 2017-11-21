7cc01cb886761e6d033bad46aa79e29935ab0e30
nicol <nicol@192.168.38.105>
2017-11-21 22:00:35 +0100

    Eksempel på tekst

---
+++ 36_del3/src/Tekst/Tekster.java
@@ -0,0 +1,15 @@
+package Tekst;
+
+	public class Tekster {
+		public final static String[] TEKSTER =
+			{
+			"Spillet starter \n",
+			"Spiller nr: %d Navn: %s, Saldo: %d \n",
+			"Venter p� spiller %s. Tast ENTER for at forts�tte \n",
+			"Terning 1: %s, Terning 2: %s \n",
+			"Status: ",
+			"Spiller %s : %d  *** ",
+			"Spiller %s : Slog to ens tillykke. Du m� sl� igen \n",
+			"Spillet er slut. %s vandt"	
+			};
+	}
--- 36_del3/src/main/Main.java
+++ 36_del3/src/main/Main.java
@@ -1,15 +1,12 @@
 package main;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("test 123");
-		System.out.println("Hello World!");
-		
-		System.out.println("test jacob");
-		System.out.println("test monica 123");
+
 		//test 123 rrrrrrrr
 		// test 222
 
@@ -18,6 +15,10 @@ public class Main {
 
 		//test nils
 		//Heprforwepå
+		
+		System.out.println(Tekst.Tekster.TEKSTER[0]);
+		
+		System.out.println(String.format(Tekst.Tekster.TEKSTER[2],"Preben"));
 	}
 
 }
