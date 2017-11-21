353714caac9785e1896821acb0ac6d265cbd65e6
nicol <nicol@LAPTOP-KMKH3SE0.eduroam.wireless.dtu.dk>
2017-11-21 10:55:46 +0100

    Spiller klasse redigeret

--- 36_del3/src/monololy_junior/Spiller.java
+++ 36_del3/src/monololy_junior/Spiller.java
@@ -22,15 +22,20 @@ public class Spiller {
 		Felt[] ejetFelter = nytFelt;
 	}
 	
-	public void checkToEns(Felt købtFelt) {
+	private void checkToEns(Felt købtFelt) {
 		for(int i = 1;i<ejetFelter.length;i++) {
 			Color farve1 = ejetFelter[i].getFeltFarve();
 			Color farve2 = købtFelt.getFeltFarve();
 			if(farve1.getRGB()==farve2.getRGB()) {
-				int købtFeltVærdi = købtFelt.getVærdi()*2;
-				købtFelt.setVærdi(købtFeltVærdi);
-				ejetFelter[i].setVærdi(købtFeltVærdi);
+			 
 			}
 		}
 	}
-}
+	private void forøgVærdi(Felt købtFelt) {
+	int forøget = købtFelt.getVærdi()*2;
+	købtFelt.setVærdi(forøget);
+	
+	}
+		
+	}
+
