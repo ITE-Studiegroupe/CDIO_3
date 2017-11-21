4623c0823fcfe48477c239b60a30fa34375334a2
Jacob <Jacob@DESKTOP-91SF1OG.eduroam.wireless.dtu.dk>
2017-11-21 11:30:38 +0100

    Startet på spillogik

--- 36_del3/src/spillogik/Spillelogik.java
+++ 36_del3/src/spillogik/Spillelogik.java
@@ -1,5 +1,14 @@
 package spillogik;
 
+import monololy_junior.Spiller;
+
 public class Spillelogik {
+	
+	final static int taberPoint = 0;
+	
+	
+	public static void CDIO3_logik(int terninger, Spiller s) {
+		
+	}
 
 }
