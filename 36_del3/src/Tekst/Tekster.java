7e74322621d7bbf5b935915d96de10fe9a300523
nicol <nicol@192.168.38.105>
2017-11-21 22:03:08 +0100

    tekst

--- 36_del3/src/Tekst/Tekster.java
+++ 36_del3/src/Tekst/Tekster.java
@@ -5,11 +5,11 @@ package Tekst;
 			{
 			"Spillet starter \n",
 			"Spiller nr: %d Navn: %s, Saldo: %d \n",
-			"Venter p� spiller %s. Tast ENTER for at forts�tte \n",
+			"Venter på spiller %s. Tast ENTER for at fortsætte \n",
 			"Terning 1: %s, Terning 2: %s \n",
 			"Status: ",
 			"Spiller %s : %d  *** ",
-			"Spiller %s : Slog to ens tillykke. Du m� sl� igen \n",
+			"Spiller %s : Slog to ens tillykke. Du må slå igen \n",
 			"Spillet er slut. %s vandt"	
 			};
 	}
