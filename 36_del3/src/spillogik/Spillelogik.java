d08f2e0d7a00afaab7c0ed40cd1d7bec94785311
Mads Schultz <s175123@student.dtu.dk>
2017-11-22 10:38:46 +0100

    Opretter atributter/metoder til spillogik.java

--- 36_del3/src/spillogik/Spillelogik.java
+++ 36_del3/src/spillogik/Spillelogik.java
@@ -1,9 +1,33 @@
 package spillogik;
 
+import controller.GUIFører;
+import monololy_junior.Plade;
+import monololy_junior.Raflebæger;
 import monololy_junior.Spiller;
 
 public class Spillelogik {
 	
+	//Spillogikkens atributter.
+	int antalSpillere;
+	int nuværendeSpiller;
+	
+	Plade plade;
+	GUIFører guiFører;	
+	Raflebæger bæger;
+	Spiller[] spillere;
+
+	//Opretter nyt plade objekt.
+	plade = new Plade();
+	
+	
+	//Opretter getter.metode til Pladen
+	public Plade getPlade() {
+		return plade;
+	}
+
+	
+
+
 	final static int taberPoint = 0;
 	
 	
