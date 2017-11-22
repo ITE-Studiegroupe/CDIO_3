948416840efe63f015d4dcdf2c996b85cf690a74
nicol <nicol@192.168.38.105>
2017-11-22 10:20:34 +0100

    TEKST

---
+++ 36_del3/src/Tekst/TekstChancekort.java
@@ -0,0 +1,15 @@
+package Tekst;
+
+public class TekstChancekort {
+	public final static String[] TEKSTER =
+		{
+		"Du skylder banken penge! Betal 3kr.",
+		"Du har lyst til at se på dyr, ryk til Zoo",
+		"Banken har fødselsdag, betal 4kr.",
+		"Du vil spille på flippermaskiner, ryk til Spillehallen",
+		"Du er kæmpe fan af Egon Olsen, ryk til Fængsel",
+		"Du har fundet 1kr på gaden, det er den lykkedag! Du får 1kr.",
+		"Du har smadret din sparegris. Du får 2kr.",
+		"Du har solgt din javabog. Du får 4kr.",	
+		};
+}
--- 36_del3/src/monololy_junior/felter/Felt_Fængsel.java
+++ 36_del3/src/monololy_junior/felter/Felt_Fængsel.java
@@ -4,11 +4,9 @@ import java.awt.Color;
 
 public class Felt_Fængsel extends Felt{
 	private boolean erFængsel;
-	private String forklaring;
 	
-	public Felt_Fængsel(String tekst, String forklaring, boolean erFængsel, Color bgFarve, Color tekstFarve) {
+	public Felt_Fængsel(String tekst, boolean erFængsel, Color bgFarve, Color tekstFarve) {
 		feltNavn = tekst;
-		this.forklaring = forklaring;
 		this.erFængsel = erFængsel;
 		this.bgFarve = bgFarve;
 		this.tekstFarve = tekstFarve;
@@ -20,14 +18,7 @@ public class Felt_Fængsel extends Felt{
 
 	public void setErFængsel(boolean erFængsel) {
 		this.erFængsel = erFængsel;
-	}
 
-	public String getForklaring() {
-		return forklaring;
-	}
-
-	public void setForklaring(String forklaring) {
-		this.forklaring = forklaring;
 	}
 
 }
