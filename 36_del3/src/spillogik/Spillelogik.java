bca091fd28160c77ce1e2b5c4f23794d649323d1
Mads Schultz <s175123@student.dtu.dk>
2017-11-22 10:49:35 +0100

    further

--- 36_del3/src/spillogik/Spillelogik.java
+++ 36_del3/src/spillogik/Spillelogik.java
@@ -6,33 +6,50 @@ import monololy_junior.Raflebæger;
 import monololy_junior.Spiller;
 
 public class Spillelogik {
-	
-	//Spillogikkens atributter.
+
+	// Spillogikkens atributter.
 	int antalSpillere;
 	int nuværendeSpiller;
-	
+
 	Plade plade;
-	GUIFører guiFører;	
+	GUIFører guiFører;
 	Raflebæger bæger;
 	Spiller[] spillere;
 
-	//Opretter nyt plade objekt.
-	plade = new Plade();
-	
-	
-	//Opretter getter.metode til Pladen
-	public Plade getPlade() {
+	// Opretter nyt plade objekt.
+	Plade pladeObjekt = new Plade();
+
+	// Opretter getter.metode til plade.
+	public Plade getBygPlade() {
 		return plade;
 	}
 
+	GUIFører guiObjekt = new GUIFører();
 	
-
+	antalSpillere == return visVælgAntalSpillere
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	final static int taberPoint = 0;
-	
-	
+
 	public static void CDIO3_logik(int terninger, Spiller s) {
-		
+
 	}
 
 }
