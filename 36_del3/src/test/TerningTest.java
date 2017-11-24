530ce789d23797f1d55a1db3ea7b08bb1852dc16
Mads Schultz <s175123@student.dtu.dk>
2017-11-24 11:31:18 +0100

    TerningTest Update

--- 36_del3/src/test/TerningTest.java
+++ 36_del3/src/test/TerningTest.java
@@ -37,7 +37,7 @@ class TerningTest {
 		int side6 = 0;
 
 		for (int i = 0; i < 10000; i++) {
-			int fordeling = terning.getVærdi();
+			int fordeling = terning.getAntalØjne();
 			if (1 == fordeling)
 				side1++;
 			else if (2 == fordeling)
