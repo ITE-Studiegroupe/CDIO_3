9527089de3a67dfd66d5ffdd749aae31b0b2525a
Mads Schultz <s175123@student.dtu.dk>
2017-11-27 12:35:27 +0100

    Kommeteret klassen.

--- 36_del3/src/main/Main.java
+++ 36_del3/src/main/Main.java
@@ -4,6 +4,8 @@ import controller.Controller;
 
 public class Main {
 
+	//Mainmetode med nyt ControllerObjekt c.
+	//Så længe while(true) er opfyldt kører spillet.
 	public static void main(String[] args) {
 		Controller c = new Controller();
 		while(true) {
