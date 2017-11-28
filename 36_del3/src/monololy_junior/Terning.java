20a9672e0de9d2a2ed11ec91e4c1888c94ac5ca5
Monica Schmidt <Monica Schmidt@DESKTOP-TU5PO0V.eduroam.wireless.dtu.dk>
2017-11-28 11:58:42 +0100

    Kommentar til Terning

--- 36_del3/src/monololy_junior/Terning.java
+++ 36_del3/src/monololy_junior/Terning.java
@@ -1,7 +1,16 @@
 package monololy_junior;
 
+/**
+ * Klasse: Terning
+ * @author Gruppe_36
+ * Klassen styrer spillerens kast med terning
+ */
 public class Terning {
 
+/**
+ * Metoden returnerer et tilfældigt tal mellem 1-6, som repræsenterer en ternings værdi
+ * @return antalØjne
+ */
 	public int kastTerning() {
 		int antalØjne = ((int) (Math.random() * 6) + 1);
 		return antalØjne;
