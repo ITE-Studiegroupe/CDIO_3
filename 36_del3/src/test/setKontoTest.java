a38a54663d7ab02b0d88cb6a03d1d2b22d5313be
Jacob <Jacob@DESKTOP-91SF1OG.hhk.dk>
2017-12-01 08:02:09 +0100

    Lavet setKonto test

---
+++ 36_del3/src/test/setKontoTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import monololy_junior.Konto;
+
+public class setKontoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Konto konto = new Konto();
+		konto.setPengeBeholdning(100);
+		int actual = konto.getPengeBeholdning();
+		int expected = 100;
+		assertEquals(actual, expected);
+	}
+
+}
